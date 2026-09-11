9fc4e6712294a79078f6ff4b7ea31bc0739fcffb
muztahid2102232 <muztahid.2102232@bau.edu.bd>
2026-09-12 00:26:33 +0600

    completed hero section without responsiveness

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,29 +1,39 @@
-import banner from '../assets/banner-stack.png';
+import banner from "../assets/banner-stack.png";
 const Hero = () => {
-    return (
-      <section className="grid grid-cols-2 items-center container mx-auto mt-24 ">
-        <div>
-          <h1 className="font-extrabold text-[60px]">
+  return (
+    <section className="lg:grid grid-cols-2 items-center container mx-auto pt-24 mb-28  ">
+      <div className="flex flex-col gap-4">
+        <div className="">
+          <h1 className="font-bold text-[50px] leading-none">
             Build Your Ideal <br />
             <span className="bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
               Development Stack
             </span>
           </h1>
-          <p>
+        </div>
+
+        <div className="">
+          <p className="text-[18px] text-[#475569] mb-6">
             Explore frontend, backend, database, and tooling options,
             <br /> compare them side by side, and put together the stack that
             fits your next project.
           </p>
-          <button className="bg-linear-to-r from-[#F97316] to-[#EC4899] rounded-lg px-4 py-3">
+        </div>
+
+        <div className="flex gap-4">
+          <button className="bg-linear-to-r from-[#F97316] to-[#EC4899] rounded-lg px-4 py-3 text-white text-[14px] font-semibold cursor-pointer">
             Explore Technologies
           </button>
-          <button>Learn More</button>
+          <button className="text-[#374151] px-12 py-3 rounded-lg outline outline-[#E5E7EB] text-[14px] cursor-pointer">
+            Learn More
+          </button>
         </div>
-        <div>
-          <img src={banner} alt="banner-pic" />
-        </div>
-      </section>
-    );
+      </div>
+      <div>
+        <img src={banner} alt="banner-pic" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo-text.png";
 
 const Nav = () => {
   return (
-    <nav className="flex gap-5 items-center justify-between mt-5 container mx-auto sticky top-0  bg-white ">
+    <nav className="flex gap-5 items-center justify-between pt-5 mx-auto sticky top-0  bg-white container">
       <div>
         <img src={logo} alt="logo-with-text" />
       </div>
@@ -26,8 +26,8 @@ const Nav = () => {
         </ul>
       </div>
       <div className="text-[14px] font-medium flex gap-5 items-center justify-center">
-        <button className="text-[#334155]">Sign In</button>
-        <button className="bg-[#D91B7E] rounded-full text-white px-5 py-2.5">
+        <button className="text-[#334155] cursor-pointer">Sign In</button>
+        <button className="bg-[#D91B7E] rounded-full text-white px-5 py-2.5 cursor-pointer">
           Sign Up
         </button>
       </div>
@@ -36,3 +36,39 @@ const Nav = () => {
 };
 
 export default Nav;
+
+<nav className="sticky top-0 z-50">
+  <div className="grid grid-cols-3 items-center px-4 py-4 md:flex md:justify-between">
+    <div className="flex justify-start">
+      <button className="md:hidden">☰</button>
+
+      <a href="#" className="hidden md:block">
+        MyLogo
+      </a>
+    </div>
+
+    <div className="flex justify-center md:hidden">
+      <a href="#">MyLogo</a>
+    </div>
+
+    <ul className="hidden items-center gap-8 md:flex">
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+      <li>
+        <a href="#">Projects</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+    </ul>
+
+    <div className="flex justify-end gap-2">
+      <button>Login</button>
+      <button>Sign Up</button>
+    </div>
+  </div>
+</nav>;
